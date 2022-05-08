c06f7752f2edd614be89d840b516a43143d40307
Fernando <fernando.luiz92@hotmail.com>
2022-05-08 19:44:14 -0300

    PersonController, BookController, MathController implements annotation @Api and @ApiOperation

--- WorkspaceSpringBoot/rest-with-springboot/src/main/java/br/com/nttdata/controller/MathController.java
+++ WorkspaceSpringBoot/rest-with-springboot/src/main/java/br/com/nttdata/controller/MathController.java
@@ -7,40 +7,49 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.nttdata.service.MathService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 
+@Api(value = "Math Endpoint", description = "Math API", tags = {"MathEndpoint"})
 @RestController
 public class MathController {
 
 	@Autowired
 	private MathService service;
 
+	@ApiOperation(value = "sum the two numbers")
 	@RequestMapping(value = "sum/{numberOne}/{numberTwo}", method = RequestMethod.GET)
 	public Double sum(@PathVariable("numberOne") String numberOne, @PathVariable("numberTwo") String numberTwo)
 			throws Exception {
 		return service.sum(numberOne, numberTwo);
 	}
 
+	@ApiOperation(value = "sub the two numbers")
 	@RequestMapping(value = "sub/{numberOne}/{numberTwo}", method = RequestMethod.GET)
 	public Double sub(@PathVariable("numberOne") String numberOne, @PathVariable("numberTwo") String numberTwo)
 			throws Exception {
 		return service.sub(numberOne, numberTwo);
 	}
 
+	@ApiOperation(value = "mul the two numbers")
 	@RequestMapping(value = "mul/{numberOne}/{numberTwo}", method = RequestMethod.GET)
 	public Double mul(@PathVariable("numberOne") String numberOne, @PathVariable("numberTwo") String numberTwo) {
 		return service.mul(numberOne, numberTwo);
 	}
 
+	@ApiOperation(value = "div the two numbers")
 	@RequestMapping(value = "div/{numberOne}/{numberTwo}", method = RequestMethod.GET)
 	public Double div(@PathVariable("numberOne") String numberOne, @PathVariable("numberTwo") String numberTwo) {
 		return service.div(numberOne, numberTwo);
 	}
 
+	@ApiOperation(value = "med the two numbers")
 	@RequestMapping(value = "med/{numberOne}/{numberTwo}", method = RequestMethod.GET)
 	public Double med(@PathVariable("numberOne") String numberOne, @PathVariable("numberTwo") String numberTwo) {
 		return service.med(numberOne, numberTwo);
 	}
 
+	@ApiOperation(value = "square root")
 	@RequestMapping(value = "square/{number}", method = RequestMethod.GET)
 	public Double square(@PathVariable("number") String number) {
 		return service.square(number);
