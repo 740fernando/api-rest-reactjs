114ba9c5b0afcafd8496b920d419d9b159073ef5
fsouviei <fernando.souzavieira@everis.nttdata.com>
2022-05-10 17:16:17 -0300

    Refactored code

--- WorkspaceSpringBoot/rest-with-springboot/src/main/java/br/com/nttdata/security/jwt/JwtTokenProvider.java
+++ WorkspaceSpringBoot/rest-with-springboot/src/main/java/br/com/nttdata/security/jwt/JwtTokenProvider.java
@@ -9,16 +9,11 @@ import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.web.authentication.Http403ForbiddenEntryPoint;
 import org.springframework.stereotype.Service;
-import org.springframework.web.server.ResponseStatusException;
-
-import com.fasterxml.jackson.core.JsonProcessingException;
 
 import br.com.nttdata.exception.InvalidJwtAuthenticationException;
 import io.jsonwebtoken.Claims;
@@ -27,13 +22,17 @@ import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 
 /**
- * Classe representa o valor padrao que será definido na securityKey e validityInMilliSeconds
- * caso nao, seja definido valor no application.properties
+ * Classe representa o valor padrao que será definido na securityKey e
+ * validityInMilliSeconds caso nao, seja definido valor no
+ * application.properties
+ * 
  * @author fsouviei
  *
  */
 @Service
 public class JwtTokenProvider {
+
+	private static final String EXPIRED_OR_INVALID_TOKEN = "Expired or invalid token";
 
 	private static final String AUTHORIZATION = "Authorization";
 
@@ -41,33 +40,29 @@ public class JwtTokenProvider {
 
 	@Value("${security.jwt.token.secret-key:secret}")
 	private String secretKey = "secret";
-	
+
 	@Value("${security.jwt.token.expire-length:3600000}")
-	private long validityInMilliseconds = 3600000; //== one hour
-	
+	private long validityInMilliseconds = 3600000; // == one hour
+
 	@Autowired
 	private UserDetailsService userDetailsService;
-	
+
 	@PostConstruct // responsavel por encodar a secret key
 	public void init() {
 		secretKey = Base64.getEncoder().encodeToString(secretKey.getBytes());
 	}
-	
+
 	// Responsavel por criar o token
 	public String createToken(String username, List<String> roles) {
 		Claims claims = Jwts.claims().setSubject(username);
-		claims.put("roles",roles);
-		
+		claims.put("roles", roles);
+
 		Date now = new Date();
 		Date validity = new Date(now.getTime() + validityInMilliseconds);
-		return Jwts.builder()
-				.setClaims(claims)
-				.setIssuedAt(now)
-				.setExpiration(validity)
-				.signWith(SignatureAlgorithm.HS256, secretKey)
-				.compact();
+		return Jwts.builder().setClaims(claims).setIssuedAt(now).setExpiration(validity)
+				.signWith(SignatureAlgorithm.HS256, secretKey).compact();
 	}
-	
+
 	public Authentication getAuthentication(String token) {
 		UserDetails userDetails = this.userDetailsService.loadUserByUsername(getUserName(token));
 		return new UsernamePasswordAuthenticationToken(userDetails, "", userDetails.getAuthorities());
@@ -76,21 +71,19 @@ public class JwtTokenProvider {
 	private String getUserName(String token) {
 		return Jwts.parser().setSigningKey(secretKey).parseClaimsJws(token).getBody().getSubject();
 	}
-	
-	public String resolveToken(HttpServletRequest req)  {
-			String bearerToken = req.getHeader(AUTHORIZATION);
-			if (bearerToken != null && bearerToken.startsWith("Bearer ")) {
-				return bearerToken.substring(7, bearerToken.length());
-			}		
-			return null;
+
+	public String resolveToken(HttpServletRequest req) {
+		String bearerToken = req.getHeader(AUTHORIZATION);
+		return (bearerToken != null && bearerToken.startsWith(BEARER)) ? bearerToken.substring(7, bearerToken.length())
+				: null;
 	}
-	
+
 	public boolean validateToken(String token) {
 		try {
 			Jws<Claims> claims = Jwts.parser().setSigningKey(secretKey).parseClaimsJws(token);
-			return (claims.getBody().getExpiration().before(new Date()))? false : true;
-		}catch(Exception e) {
-			throw new InvalidJwtAuthenticationException("Expired or invalid token");
+			return (claims.getBody().getExpiration().before(new Date())) ? false : true;
+		} catch (Exception e) {
+			throw new InvalidJwtAuthenticationException(EXPIRED_OR_INVALID_TOKEN);
 		}
 	}
 }
