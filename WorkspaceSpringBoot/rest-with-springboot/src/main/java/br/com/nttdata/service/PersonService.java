fe5d4333154507a7e9d8fb13bb5430fc0a464fae
fsouviei <fernando.souzavieira@everis.nttdata.com>
2022-05-11 10:57:11 -0300

    Implemented Pageable and Query Params

--- WorkspaceSpringBoot/rest-with-springboot/src/main/java/br/com/nttdata/service/PersonService.java
+++ WorkspaceSpringBoot/rest-with-springboot/src/main/java/br/com/nttdata/service/PersonService.java
@@ -2,12 +2,14 @@ package br.com.nttdata.service;
 
 import java.util.List;
 
+import org.springframework.data.domain.Pageable;
+
 import br.com.nttdata.data.vo.v1.PersonVO;
 
 public interface PersonService {
 	
 	PersonVO findById(Long id);
-	List<PersonVO> findAll();
+	List<PersonVO> findAll(Pageable pageable);
 	PersonVO create(PersonVO person);
 	PersonVO update(PersonVO person);
 	void delete(Long id);
