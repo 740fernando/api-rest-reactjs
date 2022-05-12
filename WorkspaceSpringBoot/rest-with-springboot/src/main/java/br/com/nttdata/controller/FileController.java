4a9cc6f325f4ce0e858ab283515b6d3c2783e262
fsouviei <fernando.souzavieira@everis.nttdata.com>
2022-05-12 15:17:00 -0300

    Created FileController

--- WorkspaceSpringBoot/rest-with-springboot/src/main/java/br/com/nttdata/controller/FileController.java
+++ WorkspaceSpringBoot/rest-with-springboot/src/main/java/br/com/nttdata/controller/FileController.java
@@ -1,8 +1,15 @@
 package br.com.nttdata.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import br.com.nttdata.data.vo.v1.UploadFileResponseVO;
+import br.com.nttdata.service.FileStorageService;
 import io.swagger.annotations.Api;
 
 @Api(tags = "FileEndpoint")
@@ -10,4 +17,19 @@ import io.swagger.annotations.Api;
 @RequestMapping("/api/file/v1")
 public class FileController {
 
+	@Autowired
+	private FileStorageService fileStorageService;
+	
+	
+	@PostMapping("/uploadFile")
+	public UploadFileResponseVO uploadFile(@RequestParam("file") MultipartFile file) {
+		String fileName = fileStorageService.storeFile(file);
+		
+		String fileDownloadUri = ServletUriComponentsBuilder.fromCurrentContextPath()
+				.path("/api/v1/downloadFile")
+				.path(fileName).
+				toUriString();
+		
+		return new UploadFileResponseVO(fileName, fileDownloadUri, file.getContentType(), file.getSize());
+	}
 }
