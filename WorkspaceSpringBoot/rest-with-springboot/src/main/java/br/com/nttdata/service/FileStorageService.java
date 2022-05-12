3f54636b1f925a93e9dbdc3216bd6c399bb84cf8
fsouviei <fernando.souzavieira@everis.nttdata.com>
2022-05-12 15:59:16 -0300

    Prepared the Service to Provide Downloads

--- WorkspaceSpringBoot/rest-with-springboot/src/main/java/br/com/nttdata/service/FileStorageService.java
+++ WorkspaceSpringBoot/rest-with-springboot/src/main/java/br/com/nttdata/service/FileStorageService.java
@@ -1,9 +1,12 @@
 package br.com.nttdata.service;
 
+import org.springframework.core.io.Resource;
 import org.springframework.web.multipart.MultipartFile;
 
 public interface FileStorageService {
 	
 	String storeFile(MultipartFile file);
+	
+	Resource loadFileAsResource(String fileName);
 
 }
