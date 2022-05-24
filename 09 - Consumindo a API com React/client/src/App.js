99c6ad234086bd0913287a3e518d09c3a85b1055
Fernando <fernando.luiz92@hotmail.com>
2022-05-24 20:15:58 -0300

    Configuring routes with React Router Dom

--- 09 - Consumindo a API com React/client/src/App.js
+++ 09 - Consumindo a API com React/client/src/App.js
@@ -2,10 +2,10 @@ import React, {useState} from 'react';
 
 import './global.css';
 
-import Login from './pages/Login';
+import Routes from './pages/Login';
 
 export default function App() {
     return(
-      <Login/>
+      <Routes/>
     );
 }
