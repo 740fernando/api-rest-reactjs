5e02c27c68b44d151b62b2659be735a5f0ea3d8b
Fernando <fernando.luiz92@hotmail.com>
2022-05-24 08:25:58 -0300

    Defining the Login Screen HTML Structure

--- 09 - Consumindo a API com React/client/src/pages/Login/index.js
+++ 09 - Consumindo a API com React/client/src/pages/Login/index.js
@@ -1,8 +1,23 @@
 import React from 'react';
-import './style.css';
+import './styles.css';
+
+import logo from '../../assets/dragon.svg'
+import padlock from '../../assets/padlock.png'
 
 export default function login() {
     return (
-        <h1>Welcome</h1>
+       <div className='login-container'>
+           <section className="form">
+                <img src={logo} alt="ntt" width="320" height="205" />
+                <form>
+                    <h1>Access your Account</h1>
+                    <input placeholder='Username' />
+                    <input type='password' placeholder='Password'/>
+
+                    <button type='submit'>Login</button>
+                </form>
+           </section>
+           <img src={padlock} alt="Login" width="320" height="205" />
+       </div>
     )
 }
