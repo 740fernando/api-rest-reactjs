8a1431dc52aba214859929ed55c9419940c3cdb1
Fernando <fernando.luiz92@hotmail.com>
2022-05-24 07:17:02 -0300

    The Concept of Property and State

--- 09 - Consumindo a API com React/client/src/App.js
+++ 09 - Consumindo a API com React/client/src/App.js
@@ -1,8 +1,11 @@
 import React from 'react';
 
+import Header from './Header';
 
 export default function App() {
   return (
-    <h1>Hello RESTful</h1>
+    <Header>
+      Client REST - Properties
+    </Header>
     );
 }
