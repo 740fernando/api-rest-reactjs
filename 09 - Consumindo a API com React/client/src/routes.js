61b94abd5032242c7db61954cae18e0765cd3d74
Fernando <fernando.luiz92@hotmail.com>
2022-05-26 21:21:31 -0300

    Integrating the Book Registration Screen with the API

--- 09 - Consumindo a API com React/client/src/routes.js
+++ 09 - Consumindo a API com React/client/src/routes.js
@@ -5,7 +5,7 @@ import Login from './pages/Login';
 import Books from './pages/Books';
 import NewBook from './pages/NewBook';
 
-export default function App(){
+export default function Routes(){
     return (
         <BrowserRouter>
             <Switch>
